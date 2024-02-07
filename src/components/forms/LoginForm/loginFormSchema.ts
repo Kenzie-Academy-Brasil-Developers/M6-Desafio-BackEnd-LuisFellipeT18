2004c6d855e74b3f49f61a1eef2b3b5cb25ca95b
LuisFellipeT18 <luisfellipe.silva@outlook.com>
2024-02-07 14:13:48 -0500

    atualizacao

--- src/components/forms/LoginForm/loginFormSchema.ts
+++
@@ -1,6 +0,0 @@
-import { z } from "zod";
-
-export const LoginFormSchema = z.object({
-    email: z.string().min(1,{message:"E-mail é obrigatória"}),
-    password: z.string().min(1,{message:"A senha é obrigatória"})
-})
