f3dafa007a82e4f00ceb5c65be272c06865a9644
LuisFellipeT18 <luisfellipe.silva@outlook.com>
2024-03-01 10:35:46 -0500

    atualizacao final

--- BackEnd/src/main.ts
+++ BackEnd/src/main.ts
@@ -8,7 +8,7 @@ import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  app.enableCors({origin:"http://localhost:5174"})
+  app.enableCors({})
   app.useGlobalPipes(
     new ValidationPipe({whitelist: true}),
     new ValidationPipe({
